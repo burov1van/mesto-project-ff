bef743449b5aa94502598d103570f9e2857b3e40
Ivan Burov <burov.i.v90@gmail.com>
2024-07-05 17:55:25 +0200

    fin

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,15 @@ const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function createCard(cards) {
+function createCard(card, deleteCard) {
   const newTemplate = cardTemplate.cloneNode(true);
 
   const cardImg = newTemplate.querySelector(".card__image");
-  cardImg.src = cards.link;
-  cardImg.alt = `Качественное фото города ${cards.name}`;
+  cardImg.src = card.link;
+  cardImg.alt = `Качественное фото города ${card.name}`;
 
   const cardTitle = newTemplate.querySelector(".card__title");
-  cardTitle.textContent = cards.name;
+  cardTitle.textContent = card.name;
 
   const deleteButton = newTemplate.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", deleteCard);
@@ -31,6 +31,6 @@ function deleteCard(event) {
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((cardData) => {
-  const cardAdd = createCard(cardData);
+  const cardAdd = createCard(cardData, deleteCard);
   cardContainer.append(cardAdd);
 });
